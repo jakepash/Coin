1849e7f08a948406faeb396a21be56b89f59c9d0
Adam Eliezerov <adameliezerov@manzanitalabs.com>
2017-08-04 16:00:12 +0300

    added site folder with our site files

--- XCodeProject/Coins/ShareViewController.swift
+++ XCodeProject/Coins/ShareViewController.swift
@@ -11,35 +11,37 @@ import UIKit
 class ShareViewController: UIViewController {
 
     @IBAction func shareButton(_ sender: Any) {
-        let firstActivityItem = "Text you want"
-        let secondActivityItem : NSURL = NSURL(string: "http//:coinreserve.io")!
-        // If you want to put an image
-        //
-        //let image : UIImage = UIImage(named: "image.jpg")!
+//        let firstActivityItem = "Text you want"
+        //        let secondActivityItem : NSURL = NSURL(string: "https://coinreserve.xyz")!
+//        // If you want to put an image
+//        //
+//        //let image : UIImage = UIImage(named: "image.jpg")!
+//
+//        let activityViewController : UIActivityViewController = UIActivityViewController(
+//            activityItems: [firstActivityItem, secondActivityItem], applicationActivities: nil)
+//
+//        // This lines is for the popover you need to show in iPad
+////        activityViewController.popoverPresentationController?.sourceView = (sender as! UIButton)
+//
+//        // This line remove the arrow of the popover to show in iPad
+//        //activityViewController.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.allZeros
+//       // activityViewController.popoverPresentationController?.sourceRect = CGRect(x: 150, y: 150, width: 0, height: 0)
+//
+//        // Anything you want to exclude
+//        activityViewController.excludedActivityTypes = [
+//            UIActivityType.postToWeibo,
+//            UIActivityType.print,
+//            UIActivityType.assignToContact,
+//            UIActivityType.saveToCameraRoll,
+//            UIActivityType.addToReadingList,
+//            UIActivityType.postToFlickr,
+//            UIActivityType.postToVimeo,
+//            UIActivityType.postToTencentWeibo
+//        ]
+//
+//        self.present(activityViewController, animated: true, completion: nil)
         
-        let activityViewController : UIActivityViewController = UIActivityViewController(
-            activityItems: [firstActivityItem, secondActivityItem], applicationActivities: nil)
         
-        // This lines is for the popover you need to show in iPad
-//        activityViewController.popoverPresentationController?.sourceView = (sender as! UIButton)
-        
-        // This line remove the arrow of the popover to show in iPad
-        //activityViewController.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.allZeros
-       // activityViewController.popoverPresentationController?.sourceRect = CGRect(x: 150, y: 150, width: 0, height: 0)
-        
-        // Anything you want to exclude
-        activityViewController.excludedActivityTypes = [
-            UIActivityType.postToWeibo,
-            UIActivityType.print,
-            UIActivityType.assignToContact,
-            UIActivityType.saveToCameraRoll,
-            UIActivityType.addToReadingList,
-            UIActivityType.postToFlickr,
-            UIActivityType.postToVimeo,
-            UIActivityType.postToTencentWeibo
-        ]
-        
-        self.present(activityViewController, animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
